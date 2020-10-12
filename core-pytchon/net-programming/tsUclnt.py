61af10b5e1c528e0eb902dbd4f2daa20871dc225
JacobZhou <ZJ@canway>
2020-10-12 20:24:39 +0800

    网络编程-socket：tcp、udp

---
+++ core-pytchon/net-programming/tsUclnt.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/12 19:35
+# @Author  : JacobZhou
+
+from socket import *
+
+HOST = 'localhost'
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+udpCliSock = socket(AF_INET, SOCK_DGRAM)
+
+while True:
+    data = input('> ')
+    if not data:
+        break
+    udpCliSock.sendto(bytes(data, 'utf-8'), ADDR)
+    data, ADDR = udpCliSock.recvfrom(BUFSIZ)
+    if not data:
+        break
+    print(data)
---
+++ core-pytchon/net-programming/tsUserv.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/12 19:27
+# @Author  : JacobZhou
+
+from socket import *
+from time import ctime
+
+HOST = ''
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+udpSerSock = socket(AF_INET, SOCK_DGRAM)
+udpSerSock.bind(ADDR)
+
+while True:
+    print('waiting for message...')
+    data, addr = udpSerSock.recvfrom(BUFSIZ)
+    s = '[%s] %s' % (ctime(), data)
+    udpSerSock.send(bytes(s, 'utf-8'), addr)
+    print('...received from and returned to: ', addr)
+
+udpSerSock.close()
